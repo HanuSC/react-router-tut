366f8cff9387fbc24cec8e5f4e62d8b53d2a7e07
HanuSC <jkalthor@gmail.com>
2023-08-24 13:16:02 -0400

    logo fix

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom'
-
+import logo from './assets/logog.png'
 const Header = () => {
   return (
     <header className='py-5 px-10 h-20 bg-orange-100 flex justify-between items-center'>
       <Link to={'/'}>{/* <h1 className='font-black text-3xl'>#VANLIFE</h1> */}
-        <img src="src/assets/logog.png" alt="logo" className='w-1/3 lg:w-40'/>
+        <img src={logo} alt="logo" className='w-1/3 lg:w-40'/>
       </Link>
       
       <nav className='flex justify-between flex-col'>
